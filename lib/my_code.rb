53f7c44bf8cf4b5d93a5ef5016a78ef77b41d2ad
Luis Pestana <lpestana@protonmail.com>
2020-03-02 15:52:58 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map (array)
+  new = []
+  i = 0
+  while i < array.length do
+    new << yield(array[i])
+    i += 1
+  end
+  new
+end
+
+def reduce (array, starting_point = nil)
+  if starting_point
+    new = starting_point
+    i = 0
+  else
+    new = array[0]
+    i = 1
+  end
+  while i < array.length do
+    new = yield(new, array[i])
+    i += 1
+  end
+  new
+end
